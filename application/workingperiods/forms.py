abb04a4b8cc712800c1acc6e119be1ad72632563
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-07 15:15:01 +0300

    Add validators and initial processing of adding a working period

--- application/workingperiods/forms.py
+++ application/workingperiods/forms.py
@@ -4,12 +4,13 @@ from wtforms import validators, IntegerField, DecimalField, DateTimeField, Selec
 
 class WorkingPeriodForm(FlaskForm):
 
-    time = DateTimeField("Date and time started")
-    length = IntegerField("Length of session in minutes")
+    time = DateTimeField("Date and time started, input in form 1.10.2013 15:20", format="%d.%m.%Y %H:%M")
+    length = IntegerField("Length of session in minutes", validators=[
+                          validators.InputRequired(), validators.NumberRange(min=0)])
     quality = DecimalField(
-        "Estimated quality of the session. 1.0 is excellent, 0.5 is average")
+        "Estimated quality of the session. 1.0 is excellent, 0.5 is average", validators=[validators.NumberRange(max=1.0, min=0.0), validators.Optional(True)])
 
-    task = SelectField("Task")
+    task = SelectField("Task", coerce=int, validators=[validators.InputRequired()])
 
     class Meta:
         csrf = False
--- application/workingperiods/views.py
+++ application/workingperiods/views.py
@@ -5,13 +5,18 @@ from application.tasks.models import Task
 from application.workingperiods.forms import WorkingPeriodForm
 
 
-@app.route("/workingperiods/new")
+@app.route("/workingperiods/new", methods=["GET", "POST"])
 @login_required
 def working_periods_new():
 
+    form = WorkingPeriodForm(request.form)
     tasks = Task.query.filter_by(account_id=current_user.id).all()
-
-    form = WorkingPeriodForm()
     form.task.choices = [(task.id, task.name) for task in tasks]
 
-    return render_template("workingperiods/new.html", form=form)
+    if request.method == "GET":
+        return render_template("workingperiods/new.html", form=form)
+
+    if not form.validate():
+        return render_template("workingperiods/new.html", form=form)
+
+    return redirect(url_for("index"))
