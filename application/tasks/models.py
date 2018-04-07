9dc622036a775325fd07f978cc939c68332b20d7
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-07 13:42:18 +0300

    Add new WorkingPeriod table, add abstract class for models

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -1,16 +1,29 @@
 from application import db
+from application.models import Base
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+
+class Task(Base):
 
     name = db.Column(db.String(144), nullable=False)
     done = db.Column(db.Boolean, nullable=False)
 
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey(
+        'account.id'), nullable=False)
 
     def __init__(self, name):
         self.name = name
         self.done = False
+
+
+class WorkingPeriod(Base):
+
+    time = db.Column(db.DateTime, nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    quality = db.Column(db.Float)
+
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'), nullable=False)
+
+    def __init__(self, time, length, quality):
+        self.time = time
+        self.length = length
+        self.quality = quality
