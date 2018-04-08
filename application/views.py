b4940a06757f7c1057010316c1ef69d92fe9d71c
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-08 17:03:15 +0300

    Delete old, redundant code

--- application/views.py
+++ application/views.py
@@ -1,24 +1,7 @@
 from flask import render_template
 from application import app
 
-class Item:
-    def __init__(self, name):
-        self.name = name
-
-nimi = "Essi Esimerkki"
-
-lista = [1, 1, 2, 3, 5, 8, 11]
-
-esineet = []
-esineet.append(Item("Eka"));
-esineet.append(Item("Toka"));
-esineet.append(Item("Kolmas"));
-esineet.append(Item("Neljäs"));
 
 @app.route("/", endpoint='index')
 def hello():
     return render_template("index.html")
-
-@app.route("/demo")
-def content():
-    return render_template("demo.html", nimi=nimi, lista=lista, esineet=esineet)
