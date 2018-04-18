e83f3cf7bbed93766d4b54b33ac9a52b071e7264
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-18 19:39:56 +0300

    Can no longer create task with name of only whitespace

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -3,14 +3,16 @@ from wtforms import StringField, validators, BooleanField
 
 
 class TaskForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=2, max=100)])
+    name = StringField("Task name", validators=[validators.Length(
+        min=2, max=100), validators.DataRequired()])
 
     class Meta:
         csrf = False
 
 
 class EditForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=2, max=100)])
+    name = StringField("Task name", validators=[validators.Length(
+        min=2, max=100), validators.DataRequired()])
     done = BooleanField("Done")
 
     class Meta:
