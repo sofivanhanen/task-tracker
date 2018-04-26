a8c8f80d416538faeca237d8af04b364da016771
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-04-26 14:49:11 +0300

    List classes

--- application/classes/models.py
+++ application/classes/models.py
@@ -1,10 +1,12 @@
 from application import db, classtask
 from application.models import Base
+from sqlalchemy.sql import text
 
 
 class Class(Base):
 
     name = db.Column(db.String(144), nullable=False)
+    count = 0
 
     account_id = db.Column(db.Integer, db.ForeignKey(
         'account.id'), nullable=False)
@@ -16,3 +18,13 @@ class Class(Base):
 
     def __init__(self, name):
         self.name = name
+        self.count = get_total_tasks_for_class(self.id)
+
+    @staticmethod
+    def get_total_tasks_for_class(class_id):
+        stmt = text(
+            "SELECT COUNT(task.id) FROM task INNER JOIN classtask ON classtask.task_id = task.id INNER JOIN class ON class.id = classtask.class_id WHERE class.id = " + str(class_id))
+        res = db.engine.execute(res)
+        for row in res:
+            # res contains only one row
+            return row[0]
--- application/classes/views.py
+++ application/classes/views.py
@@ -4,6 +4,14 @@ from flask_login import login_required
 from application.classes.models import Class
 from application.classes.forms import ClassForm
 
+@app.route("/classes/")
+@login_required
+def classes_index():
+
+    classes = Class.query.filter_by(account_id=current_user.id).all()
+
+    return render_template("classes/list.html", classes=classes)
+
 @app.route("/classes/new/", methods=["GET", "POST"])
 @login_required
 def classes_new():
